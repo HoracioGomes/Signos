e4e8e3ddf47a7a7310f219d948b2e3b9dc410ce5
Horacio <horacio.junior@ibm.com>
2022-06-10 04:45:42 -0300

    Initial Commit

---
+++ Signos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Horacio Gomes dos Santos Junior on 10/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
